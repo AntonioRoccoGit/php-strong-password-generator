a6430f89fe714f87d0d62a8be5dc4f073554fade
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-05-06 00:24:09 +0200

    fixed a bug with symbol

--- partials/functions.php
+++ partials/functions.php
@@ -4,42 +4,23 @@ session_start();
 if (isset($_GET['password-length'])) {
     $length = $_GET['password-length'];
     $want_repeat = $_GET['want-repeat'];
+    $new_array = [];
 
-    if (isset($_GET['want-letter']) && isset($_GET['want-number']) && isset($_GET['want-symbol'])) {
-        // caso tutte selezioni
-        $new_array = array_merge($lower_case_array, $upper_case_array, $number_array, $symbol_array);
-    }
-    if (isset($_GET['want-letter']) && !(isset($_GET['want-number'])) && !(isset($_GET['want-symbol']))) {
-        // caso solo lettere
+    if (isset($_GET['want-letter'])) {
         $new_array = array_merge($lower_case_array, $upper_case_array);
     }
-    if (!(isset($_GET['want-letter'])) && !(isset($_GET['want-number'])) && isset($_GET['want-symbol'])) {
-        // caso solo simboli
-        $new_array = array_merge($symbol_array);
+
+    if (isset($_GET['want-number'])) {
+        $new_array = array_merge($new_array, $number_array);
     }
-    if (!(isset($_GET['want-letter'])) && isset($_GET['want-number']) && !(isset($_GET['want-symbol']))) {
-        // caso solo numeri
-        $new_array = array_merge($number_array);
+
+    if (isset($_GET['want-symbol'])) {
+        $new_array = array_merge($new_array, $symbol_array);
     }
-    if (isset($_GET['want-letter']) && isset($_GET['want-number']) && !(isset($_GET['want-symbol']))) {
-        // lettere numeri
-        $new_array = array_merge($lower_case_array, $upper_case_array, $number_array);
-    }
-    if (isset($_GET['want-letter']) && !(isset($_GET['want-number'])) && isset($_GET['want-symbol'])) {
-        // lettere e simboli
-        $new_array = array_merge($lower_case_array, $upper_case_array, $symbol_array);
-    }
-    if (!(isset($_GET['want-letter'])) && isset($_GET['want-number']) && isset($_GET['want-symbol'])) {
-        // numeri e simboli
-        $new_array = array_merge($number_array, $symbol_array);
-    }
-    if (!(isset($_GET['want-letter'])) && !(isset($_GET['want-number'])) && !(isset($_GET['want-symbol']))) {
-        // caso tutte selezioni
-        $new_array = array_merge($lower_case_array, $upper_case_array, $number_array, $symbol_array);
-    }
-    // genero l'array completo di elementi  
 
     // gestiamo la ripetizione di caratteri
+
+    // todo refactor for in while 
     for ($i = 0; $i < $length; $i++) {
 
         $random_index = rand(0, (count($new_array) - 1));
