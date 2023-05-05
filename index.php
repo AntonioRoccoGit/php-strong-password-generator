ea712ba5dd388b0b1034a04fa74b55a2322b95dc
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-05-05 13:12:03 +0200

    added index and style

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- LINK BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- MY STYLE -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div class="container mt-4 pt-2">
+        <div class="header">
+            <h1>Strong Password Generator</h1>
+            <h2>Genera una password sicura</h2>
+
+        </div>
+    </div>
+</body>
+
+</html>
